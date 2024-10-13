cb2c379eab8b2967f64c3fc0f617b816f86e69d2
Artem Burdukovskii <nubasick456@mail.ru>
2024-10-13 16:58:09 +0300

    Add language check

--- hello.pas
+++ hello.pas
@@ -1,3 +1,14 @@
 ﻿begin
-  
+  var language := ReadString('What language do you speak?');
+  case language of
+    'russian': Print('Привет!');
+    'english': Print('Hi!');
+    'french': Print('Bonjour!');
+    'chinese': Print('嗨!');
+    'spanish': Print('hola!');
+    'german': Print('Hallo!');
+    'italian': Print('ciao!');
+    'japanese': Print('こんにちは!');
+    else Print('I dont know this language :(')
+  end;
 end.
